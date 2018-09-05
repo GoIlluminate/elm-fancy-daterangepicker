9bb99e7c1a03e4a92bec37af09445dd8a54ea552
tlentz <tcl@ksu.edu>
2018-09-05 10:18:01 -0500

    [EN-1514] get example app to load elm app

--- example/src/static/index.js
+++ example/src/static/index.js
@@ -5,5 +5,7 @@ var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );         
 require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed 
 
 // inject bundled Elm app into div#main
-var Elm = require( '../elm/Main' );
-Elm.Main.embed( document.getElementById( 'main' ) );
+const { Elm } = require('../elm/Main.elm');
+Elm.Main.init({
+  node: document.getElementById('main')
+});
